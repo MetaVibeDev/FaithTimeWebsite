dd69bdbd09f269365aba0f6b1ef3e308f086e807
wangjun <mr_wangjun@outlook.com>
2025-01-15 16:28:52 +0800

    fix:fix a bug

--- app/share/page.tsx
+++ app/share/page.tsx
@@ -1,6 +1,12 @@
+// 在文件的顶部加上 "use client"
+'use client';
+
 import Home from "@/app/page";
 
 export default function Share(){
-  window.location.href = 'faithtime://';
+  if (typeof window !== "undefined") {
+    console.log("client")
+    window.location.href = 'faithtime://';
+  }
   return <Home />;
 }
