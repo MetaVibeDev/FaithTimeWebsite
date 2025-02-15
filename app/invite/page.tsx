054ebc922fe034d47368d653e46a548e78a0ce7f
cjlala <923931989@qq.com>
2025-02-15 11:03:58 +0800

    feat: Add invite code display and handling on invite page

--- app/invite/page.tsx
+++ app/invite/page.tsx
@@ -1,17 +1,32 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import { useSearchParams } from "next/navigation";
 
 export default function InvitePage() {
   const [hasVisited, setHasVisited] = useState(false);
+  const [inviteCode, setInviteCode] = useState<string | null>(null);
+  const searchParams = useSearchParams();
 
   useEffect(() => {
+    const code = searchParams.get("inviteCode");
+    setInviteCode(code);
+
     const visited = localStorage.getItem("hasVisitedInvitePage") === "true";
     setHasVisited(visited);
     if (!visited) {
       localStorage.setItem("hasVisitedInvitePage", "true");
     }
-  }, []);
+  }, [searchParams]);
+
+  const handleStartPraying = () => {
+    // if (inviteCode) {
+    //   window.location.href = `https://www.google.com?code=${inviteCode}`;
+    // } else {
+    //   window.location.href = "faithtime://";
+    // }
+    window.location.href = "faithtime://";
+  };
 
   return (
     <div
@@ -29,17 +44,25 @@ export default function InvitePage() {
             src="/invite/start-praying-button.png"
             alt="how to pray"
             className="w-full absolute bottom-0 animate-pulse-scale"
+            onClick={handleStartPraying}
           />
         </div>
-        <img
-          src={
-            hasVisited
-              ? "/invite/claim-rewards-old.png"
-              : "/invite/claim-rewards.png"
-          }
-          alt="claim rewards"
-          className="w-full"
-        />
+        <div className="relative w-full">
+          <img
+            src={
+              hasVisited
+                ? "/invite/claim-rewards-old.png"
+                : "/invite/claim-rewards.png"
+            }
+            alt="claim rewards"
+            className="w-full"
+          />
+          {inviteCode && (
+            <div className="absolute bottom-7 left-1/2 transform -translate-x-1/2 text-[#FFB600] font-bold text-[18px]">
+              {inviteCode}
+            </div>
+          )}
+        </div>
         <img
           src="/invite/how-to-pray.png"
           alt="how to pray"
