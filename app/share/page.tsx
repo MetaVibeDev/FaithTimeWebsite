995004efba36502ebf67d1d2d57ee1067d5578bc
cjlala <923931989@qq.com>
2025-02-23 19:21:58 +0800

    feat: Implement deep link to specific prayer request share

--- app/share/page.tsx
+++ app/share/page.tsx
@@ -1,12 +1,16 @@
-// 在文件的顶部加上 "use client"
-'use client';
+"use client";
 
 import Home from "@/app/page";
+import { useSearchParams } from "next/navigation";
+import { useEffect } from "react";
 
-export default function Share(){
-  if (typeof window !== "undefined") {
-    console.log("client")
-    window.location.href = 'faithtime://';
-  }
+export default function Share() {
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    const postID = searchParams.get("postID");
+    window.location.href = `faithtime://prayer-request-share/${postID}`;
+  }, [searchParams]);
+
   return <Home />;
 }
